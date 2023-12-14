c05662a7328bcaba8ad767810454ea96c1e12cc0
Dave van der Meer <dave.vandermeer@uni.lu>
2023-12-14 14:17:29 +0100

    Make file pylint conform.

--- launch/optitrack_tf.launch.py
+++ launch/optitrack_tf.launch.py
@@ -4,6 +4,9 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    """
+    Generate launch description to start the tf broadcaster node for OptiTrack.
+    """
     # Get the path to the broadcast_tf package
     package_dir = get_package_share_directory('optitrack_tf')
 
@@ -12,8 +15,8 @@ def generate_launch_description():
     package_dir,
     'config',
     'params.yaml'
-    )    
-    
+    )
+
     # Create a Node object to launch the broadcast_tf executable
     node = Node(
         package='optitrack_tf',
@@ -21,9 +24,9 @@ def generate_launch_description():
         name='broadcast_tf_node',
         parameters=[parameters_file_path]
     )
-    
+
     # Create the LaunchDescription object and add the Node object to it
     ld = LaunchDescription()
     ld.add_action(node)
-    
+
     return ld
